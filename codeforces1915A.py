7a12210ea049d472ba4c6f0042877036fe20da88
Era Aich <45397193+Era-Aich@users.noreply.github.com>
2025-08-29 11:24:41 +0600

    Add files via upload

---
+++ codeforces1915A.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 29 11:18:45 2025
+
+@author: asus
+"""
+
+a,b,c = map(int,input().split())
+
+if b==c:
+    print(a)
+elif a==c:
+    print(b)
+else:
+    print(c)
